01a4ccfaf077f67851247d948f5f6ee565e9e11b
saidbouchniba <saidbouchniba8@gmail.com>
2024-09-12 17:14:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      FullName: "SAID",
+      Bio: "BLACK LIVES MATTER",
+      Img: "https://t4.ftcdn.net/jpg/05/04/34/37/360_F_504343758_se6qzK6lJ2FHybEB3f4Ol0ZgIstHeWoR.jpg",
+      Profession: "STUDENT",
+      Show: false
+    }
+  }
+  HideProfile=()=>{
+    this.setState({Show:!this.state.Show})
+
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.HideProfile}>{this.state.Show == true ? "Hide" : "Show"} Profile</button>
+        {this.state.Show == true ? <div>
+          <h1>{this.state.FullName}</h1>
+          <h4>{this.state.Bio}</h4>
+          <img src={this.state.Img} />
+          <p>{this.state.Profession}</p>
+        </div> : null}
+      </div>
+    )
+  }
 }
 
-export default App;
+
+
